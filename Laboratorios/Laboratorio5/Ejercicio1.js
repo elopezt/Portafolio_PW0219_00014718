9679636aeb5b7e3c07b620e12938ce9e1535db2c
elopezt <00014718@uca.edu.sv>
2019-09-27 09:25:22 -0600

    Labo05

---
+++ Laboratorios/Laboratorio5/Ejercicio1.js
@@ -0,0 +1,19 @@
+var arr = [1, "hola", 3, true, [1, 2, 3], "hola2"];
+
+var funcion = x => {
+  let typeArr = [];
+  let counts = {};
+
+  x.forEach(element => {
+    typeArr.push(typeof element);
+  });
+
+  typeArr.forEach(element => {
+    counts[element] = (counts[element] || 0) + 1;
+  });
+
+  console.log(typeArr);
+  console.log(counts);
+};
+
+funcion(arr);
